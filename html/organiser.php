84d3444bfa627703447a3ff03631a90660f3adc8
Martin Lobel <martin.lobel@isen-ouest.yncrea.fr>
2022-06-17 09:26:41 +0200

    organiser html/css

--- html/organiser.php
+++ html/organiser.php
@@ -10,11 +10,94 @@
 
 
 <body>
-    <nav class="navbar navbar-expand-lg" style = "background: #FFA800 ; border : 3px solid #FF7A00"  >
+    <nav class="navbar navbar-expand-lg" style = "background: #FFA800 ; border : 3px solid #FF7A00"  >  
         <div class="container-fluid">
             <img src="../images/coupe.png" width="4%" height="100%">
+            <div style = "margin-left: auto;">
+                <a href = "choix.php" class="btn btn-primary" type="submit" style = "color:white;background: #4F5B5E; ">Menu</a>
+                <a href = "stats.php" class="btn btn-primary" type="submit" style = "color:white;background: #4F5B5E; ">Stats</a>
+                <a href = "connexion.php" class="btn btn-primary" type="submit" style = "color:white;background: #4F5B5E; ">Déconnection</a>
+            </div>
         </div>
     </nav>
+    <br>
+    <section class="centrage">
+            <div class="mask d-flex align-items-center h-150 gradient-custom-3 mx-2">
+                <div class="container" style="border-radius: 10px; background-color: #4F5B5E; top : 50px; margin-left: auto; margin-right: auto;">
+                    <div class="card-body p-4">
+                        <h3 class="text-uppercase text-center mb-5" style="color: #FFFFFF;">Créer un match</h3>
+                        <div class="row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Ville" id="city">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Nombre de joueurs min" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Nombre de joueurs max" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br><div class="col-12">
+                                <input class="form-control" type="text" placeholder="Adresse" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Ville" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Heure" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Durée" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+                            <div class="col-12">
+                                <input class="form-control" type="text" placeholder="Prix" id="">
+                            </div>
+                            <br><br>
+                            <div class="row">
+                                <p></p>
+                            </div>
+                            <br><br>
+
+    
+                            <button id="search" class="btn btn-primary" type="submit"
+                                style="background: #FFA800; margin-left: auto; margin-right: auto;">Rechercher</button>
+                        </div>
+                    </div>
+                </div>
+        </section>
 
 
 
